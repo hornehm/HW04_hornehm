9f46279fd2d4afceefcec358b25dc4a7c3bea096
Heather H <hornehm@muohio.edu>
2012-11-13 06:08:14 -0500

    FINAL COMMIT
    Tried to get nearestMap working, but didn't. Added comments.

--- include/HW04_hornehmApp.h
+++ include/HW04_hornehmApp.h
@@ -14,6 +14,10 @@ using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+/*
+*CensusEntry:
+* Location x, and y, plus the population for those coordinates
+*/
 class CensusEntry{
 public:
 	int population;
@@ -23,15 +27,25 @@ public:
 /*
 *@author: Heather Horne
 *
-*Homework 4 app for phase 2
+*Homework 4 app for phase 3
 *
-*For this phase, I decided to try the binary tree, however, I did not implement
-* correctly. Since I quicksorted the array and then imported entries into the tree 
-* in that order, I am pretty sure the left and right pointers from the node look 
-* like lists.
+*For this phase, I implemented A, B, possibly F, H, and Youtube video. I had a hard time
+*with making the regions. I wanted to color each pixel according to the getNearest method
+*for each pixel, but for one, it took 7 minutes to load, and it also did not color each
+*pixel.
 *
-*Everything for this phase is in the setup() method
+*Brown-Represents starbucks locations
+*Cyan-Represents the nearest starbucks when you left click on teh map
+*Light Green-High area of population
+*Medium Green-Medium area of population
+*Dark Green- low area of population
 *
+*My app also displays the location of the nearest starbucks when clicked.
+*
+*Note: You can right click to get the image map of the US to pop up. Was not able to do
+*much with it, but show where everything is.
+*
+*YOUTUBE LINK: http://youtu.be/53ahmW_Ich4
 */
 class HW04_hornehmApp : public AppBasic {
   public:
@@ -40,39 +54,47 @@ class HW04_hornehmApp : public AppBasic {
 	void update();
 	void draw();
 	void prepareSettings(Settings* settings);
+	
+	//Phase 2 methods
 	Entry* readInFile();
-	CensusEntry* readInCensus(string filename);
 	void quickSort(int start, int end);
 	void swap(int index1, int index2);
 	void delEntries();
+
+	//Phase 3 methods
+	CensusEntry* readInCensus(string filename);
 	void drawMap(uint8_t* pixels, node* r);
-	void drawPopulationMap(uint8_t* pixels, CensusEntry* arr, int items, Color8u color);
+	void drawPopulationMap(uint8_t* pixels, CensusEntry* arr, int items);
 	void drawRectangle(uint8_t* pixels, int x, int y, int width, int height, Color8u color);
 	void nearestMap(uint8_t* pixels);
-	void keyDown( KeyEvent event);
 	int reflectY(int y);
 	
 
 private:
-
+	//Surface and dataArray for map
 	Surface* mySurface;
 	uint8_t* dataArray;
 
+	//Array of entries for each file
 	CensusEntry* census2000;
 	CensusEntry* census2010;
 
+	//Variables for the added map
 	gl::Texture map;
 	bool showMap, showMessage;
 	string message;
 
+	//Widths/Height/Texturesize
 	static const int appWidth = 600;
 	static const int appHeight = 400;
 	static const int textureSize = 1024;
+
 
 	Entry* starbucks;//entry array
 	int numItems, numItems2010, numItems2000;//number of items in the array
 	hornehmStarbucks* stores;//used to build tree
 
+	//Font variables for message
 	Font f;
 	gl::TextureFontRef textFont;
 	
--- src/HW04_hornehmApp.cpp
+++ src/HW04_hornehmApp.cpp
@@ -46,6 +46,12 @@ Entry* HW04_hornehmApp::readInFile(){
 	return test;
 }
 
+
+/*
+*Imports x, y, and population from the census files.
+*
+*@param filename - either "Census_2010.csv" or "Census_2000.csv".. or whatever else
+*/
 CensusEntry* HW04_hornehmApp::readInCensus(string filename){
 	int pop;
 	double x, y;
@@ -147,7 +153,7 @@ void HW04_hornehmApp::quickSort(int start, int end){
 }
 
 /*
-*Swaps the data of two indexes (index1 and index2)
+*Swaps the data of two indices (index1 and index2)
 *
 */
 void HW04_hornehmApp::swap(int index1, int index2){
@@ -165,6 +171,17 @@ void HW04_hornehmApp::delEntries(){
 	delete [] starbucks;
 }
 
+/*
+*Draw rectangle for each starbucks location. I thought this was nice to see the points a little better.
+*I used this same method from my first homework.
+*
+*@param pixels - dataArray
+*@param x - top-left x coordinate
+*@param y - top-left y coordinate
+*@param width - width of rectangle
+*@param height - height of rectangle
+*@param color - color of rectangle
+*/
 void HW04_hornehmApp::drawRectangle(uint8_t* pixels, int x, int y, int width, int height, Color8u color){
 	y = reflectY(y);
 	for(int i = y; i<= y+height; i++){
@@ -176,22 +193,36 @@ void HW04_hornehmApp::drawRectangle(uint8_t* pixels, int x, int y, int width, in
 	}
 }
 
+/*
+*Draws the starbucks locations (They are brown. You can change the size of them
+* by changing the width and height of the triangle)
+*
+*@param pixels - dataArray
+*@param r - root node
+*/
 void HW04_hornehmApp::drawMap(uint8_t* pixels, node* r){
 	if (r == NULL)
 		return;
 	drawMap(pixels, r->left);
-	drawRectangle(pixels, (int)(r->data->x*appWidth), (int)(r->data->y*appHeight), 3, 3, Color8u(255,255,0));
+	drawRectangle(pixels, (int)(r->data->x*appWidth), (int)(r->data->y*appHeight), 2, 2, Color8u(218,165,32));
 	drawMap(pixels, r->right);
 	//nearestMap(pixels);
 }
 
-void HW04_hornehmApp::drawPopulationMap(uint8_t* pixels, CensusEntry* arr, int items, Color8u color){
+/*
+*Colors each pixel according to how many people at that point
+*@param pixels - dataArray
+*@param arr - Array of CensusEntries
+*@param items - number of items in arr
+*/
+void HW04_hornehmApp::drawPopulationMap(uint8_t* pixels, CensusEntry* arr, int items){
+	Color8u color;
 	for(int i = 0; i<items; i++){
-		if(arr[i].population < 1000){
+		if(arr[i].population < 1500){
 			color = Color8u(34,139,34);
 			drawRectangle(pixels, (int)(arr[i].x*appWidth), (int)(arr[i].y*appHeight), 1, 1, color);
 		}
-		else if(arr[i].population < 5000){
+		else if(arr[i].population < 3000){
 			color = Color8u(50,205,50);
 			drawRectangle(pixels, (int)(arr[i].x*appWidth), (int)(arr[i].y*appHeight), 1, 1, color);
 		}
@@ -202,10 +233,31 @@ void HW04_hornehmApp::drawPopulationMap(uint8_t* pixels, CensusEntry* arr, int i
 	}
 }
 
+/*
+*This method is supposed to color each pixel in the map according to the color of the starbucks
+* location. It doesn't work, however, in order to test it out, the values for color need to be random 
+* when the map is originally drawn.
+*
+*@param pixels - dataArray
+*/
 void HW04_hornehmApp::nearestMap(uint8_t* pixels){
-	
+	int x2, y2;
+	for(int y = 0; y < appHeight; y++){
+		for(int x = 0; x < appWidth; x++){
+			Entry* tmp = stores->getNearest((double)x/((double)appWidth),((double)y)/((double)appHeight));
+			x2 = (int)(tmp->x*appWidth);
+			y2 = (int)(tmp->y*appHeight);
+			pixels[3*(x+y*textureSize)] = pixels[3*(x2+y2*textureSize)];
+			pixels[3*(x+y*textureSize)+1] = pixels[3*(x2+y2*textureSize)+1];
+			pixels[3*(x+y*textureSize)+2] = pixels[3*(x2+y2*textureSize)+2];
+		}
+	}
 }
-
+/*
+*Reflects the Y value so that it will draw map correctly
+*
+*@param y - y value to be reflected
+*/
 int HW04_hornehmApp::reflectY(int y){
 	if(y<appHeight/2){
 		y = y+2*(abs(y-appHeight/2));
@@ -216,15 +268,6 @@ int HW04_hornehmApp::reflectY(int y){
 	return y;
 }
 
-void HW04_hornehmApp::keyDown(KeyEvent event){
-	if(event.getChar()=='1'){
-		nearestMap(dataArray);
-	}
-	if(event.KEY_ESCAPE){
-		showMessage = false;
-	}
-}
-
 void HW04_hornehmApp::prepareSettings(Settings* settings){
 	(*settings).setWindowSize(appWidth, appHeight);
 	(*settings).setResizable(false);
@@ -232,17 +275,19 @@ void HW04_hornehmApp::prepareSettings(Settings* settings){
 
 void HW04_hornehmApp::setup()
 {
+	//loads image
 	Surface surf(loadImage("../resources/usa.png"));
 	map = gl::Texture(surf);
+	//sets messages and map to false
 	showMessage = false;
 	showMap = false;
-
+	//initialize font
 	f = Font( "Arial", 24 );
 	textFont = gl::TextureFont::create(f);
 
 	numItems2000 = numItems2010 = numItems = 0;
-	census2000 = readInCensus("Census_2000.csv");
-	//census2010 = readInCensus("Census_2010.csv");
+	//census2000 = readInCensus("Census_2000.csv");
+	census2010 = readInCensus("Census_2010.csv");
 	
 	starbucks = readInFile();//reads in csv file into entry arrays
 	quickSort(0, numItems-1);//quicksort on the array to find median
@@ -250,26 +295,30 @@ void HW04_hornehmApp::setup()
 	stores->build(starbucks, numItems);//build binary search tree
 	delEntries();//delete array of entries
 	
+	//Draw map
 	mySurface = new Surface(textureSize, textureSize, false);
 	dataArray = (*mySurface).getData();
-	drawPopulationMap(dataArray, census2000, numItems2000, Color8u(0, 255, 0));
-	//drawPopulationMap(dataArray, census2010, numItems2010, Color8u(255, 0, 0));
+	//drawPopulationMap(dataArray, census2000, numItems2000);
+	drawPopulationMap(dataArray, census2010, numItems2010);
 	drawMap(dataArray, stores->root);
+	
 }
 
 void HW04_hornehmApp::mouseDown( MouseEvent event )
 {
+	//If left is down, highlight nearest starbucks at that point
 	if(event.isLeftDown()){
 		drawMap(dataArray, stores->root);
 		double x = (double)event.getX();
 		int y = event.getY();
 		y = reflectY(y);
 		Entry* tmp = stores->getNearest(x/((double)appWidth),((double)y)/((double)appHeight));
-		drawRectangle(dataArray, (int)(tmp->x*appWidth), (int)(tmp->y*appHeight), 3, 3, Color8u(0, 0, 255));
+		drawRectangle(dataArray, (int)(tmp->x*appWidth), (int)(tmp->y*appHeight), 3, 3, Color8u(0,255,255));
 		message = tmp->identifier;
 		showMessage = true;
 		
 	}
+	//if right is down, show the image in resources
 	if(event.isRightDown()){
 		showMap =!showMap;
 	}
@@ -289,6 +338,8 @@ void HW04_hornehmApp::draw()
 		gl::draw(map, getWindowBounds());
 	}
 	else{
+		//Displays the location of the starbucks
+		// when getNearest is called.
 		gl::clear(Color(0, 0, 0));
 		gl::draw(*mySurface);
 		gl::color(Color8u(255, 255, 255));
